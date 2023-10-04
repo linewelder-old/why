232119654ec4b3c209a8bb510f46d4fc85a4e1f7
linewelder <redalerm@gmail.com>
2023-10-04 22:56:14 +0200

    Disable "property can be made init only" warning in RichTextTagHelper.cs

--- Why/TagHelpers/RichTextTagHelper.cs
+++ Why/TagHelpers/RichTextTagHelper.cs
@@ -8,6 +8,8 @@ namespace Why.TagHelpers;
 [HtmlTargetElement("rich-text", Attributes = "text")]
 public class RichTextTagHelper : TagHelper
 {
+    // Required to be settable by Razor pages.
+    // ReSharper disable once PropertyCanBeMadeInitOnly.Global
     public required string Text { get; set; }
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
