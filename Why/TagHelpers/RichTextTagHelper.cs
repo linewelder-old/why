1a4746b2b57da9ef7abbadb732013e7d27cc8b13
linewelder <redalerm@gmail.com>
2023-10-04 22:51:10 +0200

    Create RichTextTagHelper

---
+++ Why/TagHelpers/RichTextTagHelper.cs
@@ -0,0 +1,147 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Why.TagHelpers;
+
+/// <summary>
+/// Generate HTML based on post formatting syntax.
+/// </summary>
+[HtmlTargetElement("rich-text", Attributes = "text")]
+public class RichTextTagHelper : TagHelper
+{
+    public required string Text { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.Attributes.Clear();
+        output.Attributes.Add("class", "lead");
+
+        var cursor = new Cursor(Text, output);
+        while (!cursor.IsDone)
+        {
+            if (cursor.TryConsume("###"))
+            {
+                var content = cursor.ConsumeUntil("###");
+                if (content.IsEmpty)
+                {
+                    output.Content.AppendHtml("###");
+                    continue;
+                }
+
+                output.Content.AppendHtml("""<h2 class="display-6"><marquee>""");
+                output.Content.Append(content.ToString());
+                output.Content.AppendHtml("</marquee></h2>");
+
+                cursor.Advance(3);
+                cursor.TryConsumeNewLine();
+            }
+            else if (cursor.TryConsume("!["))
+            {
+                var url = cursor.ConsumeUntil("]").ToString();
+                if (url.Length == 0 || !Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+                {
+                    output.Content.AppendHtml("![");
+                    output.Content.Append(url);
+                    continue;
+                }
+
+                output.Content.AppendHtml($"""<img alt="Attached image" src="{url}"/>""");
+
+                cursor.Advance(1);
+                cursor.TryConsumeNewLine();
+            }
+            else if (cursor.TryConsumeNewLine())
+            {
+                output.Content.AppendHtml("<br/>");
+            }
+            else
+            {
+                output.Content.Append(cursor.CurrentChar.ToString());
+                cursor.Advance(1);
+            }
+        }
+    }
+
+    private ref struct Cursor
+    {
+        public ReadOnlySpan<char> Text { get; init; }
+        public int CurrentIndex { get; set; }
+
+        public bool IsDone => CurrentIndex == Text.Length;
+        public char CurrentChar => Text[CurrentIndex];
+
+        public Cursor(ReadOnlySpan<char> text, TagHelperOutput output)
+        {
+            Text = text;
+        }
+
+        /// <summary>
+        /// Increment <see cref="CurrentIndex"/> by <paramref name="amount"/>.
+        /// </summary>
+        public void Advance(int amount)
+        {
+            CurrentIndex += amount;
+        }
+
+        /// <summary>
+        /// If the text contains <paramref name="substring"/> starting at
+        /// <see cref="CurrentIndex"/>, skip over it and return true,
+        /// otherwise, return false.
+        /// </summary>
+        public bool TryConsume(ReadOnlySpan<char> substring)
+        {
+            if (CurrentIndex > Text.Length - substring.Length ||
+                !Text.Slice(CurrentIndex, substring.Length)
+                    .SequenceEqual(substring))
+            {
+                return false;
+            }
+
+            Advance(substring.Length);
+            return true;
+
+        }
+
+        /// <summary>
+        /// If there is a new line ('\n', '\r', '\r\n') at <see cref="CurrentIndex"/>,
+        /// skip over it.
+        /// </summary>
+        /// <returns>True if there was a new line, false otherwise.</returns>
+        public bool TryConsumeNewLine()
+        {
+            if (IsDone) return false;
+
+            if (TryConsume("\r\n"))
+            {
+                return true;
+            }
+
+            if (CurrentChar is not ('\n' or '\r'))
+            {
+                return false;
+            }
+
+            Advance(1);
+            return true;
+
+        }
+
+        /// <summary>
+        /// Search for <paramref name="closing"/> starting at <see cref="CurrentIndex"/>.
+        /// If found, advance up to it and return the skipped over part.
+        /// Otherwise, return an empty span.
+        /// </summary>
+        public ReadOnlySpan<char> ConsumeUntil(ReadOnlySpan<char> closing)
+        {
+            var consumedLength = Text[CurrentIndex..].IndexOf(closing, StringComparison.Ordinal);
+            if (consumedLength < 0)
+            {
+                return ReadOnlySpan<char>.Empty;
+            }
+
+            var result = Text.Slice(CurrentIndex, consumedLength);
+            Advance(consumedLength);
+            return result;
+        }
+    }
+}
---
+++ WhyTests/RichTextTagHelperTests.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Why.TagHelpers;
+
+namespace WhyTests;
+
+public class RichTextTagHelperTests
+{
+    public static readonly IEnumerable<object[]> NewLines = new List<string[]>
+    {
+        new[] { "\n" },
+        new[] { "\r" },
+        new[] { "\r\n" }
+    };
+
+    [Fact]
+    public void TextGetsHtmlEncoded()
+    {
+        AssertTransformation(
+            "<h1>test</h1>",
+            "&lt;h1&gt;test&lt;/h1&gt;");
+    }
+
+    [Theory, MemberData(nameof(NewLines))]
+    public void NewLinesAreReplacedWithBr(string newLine)
+    {
+        AssertTransformation(
+            $"{newLine}one{newLine}two{newLine}",
+            "<br/>one<br/>two<br/>");
+    }
+
+    [Fact]
+    public void Marquee_Works()
+    {
+        AssertTransformation(
+            "###Marquee<3###",
+            HtmlMarquee("Marquee&lt;3"));
+    }
+
+    [Fact]
+    public void Marquee_UnclosedIsLeftAsIs()
+    {
+        AssertTransformation(
+            "###Marquee",
+            "###Marquee");
+    }
+
+    [Theory, MemberData(nameof(NewLines))]
+    public void Marquee_NoExtraLineBreakAddedAfter(string newLine)
+    {
+        AssertTransformation(
+            $"###Marquee###{newLine}some text",
+            $"{HtmlMarquee("Marquee")}some text");
+    }
+
+    [Fact]
+    public void Image_Works()
+    {
+        AssertTransformation(
+            "![https://example.com/image.png]",
+            HtmlImage("https://example.com/image.png"));
+    }
+
+    [Fact]
+    public void Image_UnclosedIsLeftAsIs()
+    {
+        AssertTransformation(
+            "![https://example.com/image.png",
+            "![https://example.com/image.png");
+    }
+
+    [Theory, MemberData(nameof(NewLines))]
+    public void Image_NoExtraLineBreakAddedAfter(string newLine)
+    {
+        AssertTransformation(
+            $"###Marquee###{newLine}some text",
+            $"{HtmlMarquee("Marquee")}some text");
+    }
+
+    [Fact]
+    public void Image_WithIncorrectUrlIsLeftAsIs()
+    {
+        AssertTransformation(
+            "![https://example.com/image.png\"]",
+            "![https://example.com/image.png&quot;]");
+    }
+
+    private static void AssertTransformation(string markupString, string expectedContent)
+    {
+        var tagHelperContext = new TagHelperContext(
+            tagName: "rich-text",
+            allAttributes: new TagHelperAttributeList(),
+            items: new Dictionary<object, object>(),
+            uniqueId: "test");
+
+        var output = new TagHelperOutput(
+            "div",
+            attributes: new TagHelperAttributeList(),
+            getChildContentAsync: (_, _) =>
+                Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+        var tagHelper = new RichTextTagHelper
+        {
+            Text = markupString
+        };
+
+        tagHelper.Process(tagHelperContext, output);
+
+        Assert.Equal("div", output.TagName);
+        Assert.Equal(expectedContent, output.Content.GetContent());
+    }
+
+    private static string HtmlMarquee(string content) =>
+        $"""<h2 class="display-6"><marquee>{content}</marquee></h2>""";
+
+    private static string HtmlImage(string url) =>
+        $"""<img alt="Attached image" src="{url}"/>""";
+}
